3eae11901c8d19759385f979a16ec924b715e7f3
Tomoki Furuhashi <mokmok511@gmail.com>
2017-08-13 12:46:07 +0900

    finish chap07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+
+  def index
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_path(@user.id)
+      # redirect_to user_path(@user)
+      redirect_to @user # => "users/#{@user.id}"
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end

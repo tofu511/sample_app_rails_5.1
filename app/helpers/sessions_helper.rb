1f73202d740991fcf60a01031453ab08ed8d09cb
Tomoki Furuhashi <mokmok511@gmail.com>
2017-08-13 19:10:03 +0900

    finish chap09

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,15 @@ module SessionsHelper
 
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) # => User.findだとセッション切れの場合にエラーを返してしまうのでnilを返すfind_byを使う
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -17,8 +25,23 @@ module SessionsHelper
 
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # ユーザーを永続的セッションを復元できるようにする
+  def remember(user)
+    user.remember # => DBにremember_digestを保存
+    cookies.permanent.signed[:user_id] = user.id # 署名付きIDをクッキーに保存
+    cookies.permanent[:remember_token] = user.remember_token # 記憶トークンをクッキーに保存
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget # => DBのremember_digestをnilにする
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
 end
